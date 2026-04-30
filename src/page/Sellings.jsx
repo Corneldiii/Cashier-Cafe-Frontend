0253ac8bf94a902a42f97732734e59f18343ceff
Your Name <you@example.com>
2026-04-30 14:00:26 +0700

    page(sellings) : Added a Sellings page to display the daily recap of sales items

---
+++ src/page/Sellings.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import Navbar from '../components/Navbar'
+
+
+const Sellings = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Sellings
